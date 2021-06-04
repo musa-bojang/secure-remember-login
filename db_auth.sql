bcba1c6f2841c22921197a3f390a5425f1e527db
Musa Bojang <musabojang413@gmail.com>
2021-06-04 21:55:33 +0800

    reference remember me radio button php login

--- db_auth.sql
+++ db_auth.sql
@@ -1,6 +1,3 @@
-# secure-remember-login
-Login Script with ‘Remember Me’ feature will allow the user to preserve their logged in status. When the user checks the Remember Me option, then the logged in status is serialized in the PHP session or cookies like storages.
-
 --
 -- Database: `db_auth`
 --
